38215bdbfdb69ac1f284e4d66aa2babd6f569281
kimgh06 <kkh061101@naver.com>
2024-03-14 15:02:29 +0900

    init

---
+++ js.js
@@ -0,0 +1,14 @@
+let name = '강현 킴';
+const age = 19;
+var is_man = true;
+
+console.log(name, age, is_man);
+console.log(typeof name, typeof age, typeof is_man);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+let obj = {
+  'rnwhcp': 29,
+  'asoifweiofjiowejfiowjfiowejfio': 3
+}
+
+console.log(numbers, obj)
